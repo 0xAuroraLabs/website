34c7e93e4c8656db791b13d90a87e3b57d0dcf46
Divye Prakash <divye.prakash07@gmail.com>
2025-09-08 17:05:43 +0530

    Bkc hata di

--- api/config.js
+++ api/config.js
@@ -3,18 +3,4 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Return environment variables
-  res.status(200).json({
-    firebaseConfig: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
-    },
-    mvpLaunchDate: process.env.MVP_LAUNCH_DATE
-  });
 } 
